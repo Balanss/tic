df010d6046b774450a09028a37acbc50b5c47a3e
Balans <gilmonpurry@hotmail.com>
2023-04-15 22:08:25 -0300

    'fix'

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:"https://balanss.github.io/tictacttoe/",
+   base:"https://balanss.github.io/tic/",
   plugins: [react()]
 })
