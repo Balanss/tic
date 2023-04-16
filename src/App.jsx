9931bd7ef13de1363afe268932ba3e747107b17e
Balans <gilmonpurry@hotmail.com>
2023-04-15 22:44:24 -0300

    trying hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState,lazy,Suspense } from 'react'
-import { Routes, BrowserRouter as Router,Route } from 'react-router-dom'
+import { Routes, HashRouter,Route } from 'react-router-dom'
 import "./App.css"
 
 import Game from './Components/Game'
@@ -10,13 +10,13 @@ const Create = lazy(() => import ("./Components/Create"))
 
   return (
     <div className="App">
-<Router> <Suspense fallback={<h2> Loading ... </h2>}>  
+<HashRouter> <Suspense fallback={<h2> Loading ... </h2>}>  
   <Routes>
    <Route path="/" element={<Create/>}></Route>
    <Route path="/game" element={<Game/>}></Route>
   </Routes>
    </Suspense>
-</Router>
+</HashRouter>
     </div>
   )
 }
